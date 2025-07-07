85cd43dbb2ac4e5bda2e57b01822729b5c89fe63
NIKITAPRO965 <nikita.pro.965@gmail.com>
2025-07-07 23:35:14 +0300

    ?

---
+++ js/work.js
@@ -0,0 +1,44 @@
+// //let,const,var
+// const firstName = "Nikita";
+// console.log(firstName);
+// let lastName;
+// lastName = "Pro"
+// console.log(lastName);
+
+// //7 примітивних типів данних: string, number, bigint, boolean, symbol, null, undefined
+// const name = "string";
+// console.log(typeof (name));
+// const age = 965;
+// console.log(typeof (age));
+// console.log(10>5);
+// console.log(965<5);
+// console.log(typeof (965<5));
+// console.log(typeof ($));
+
+
+const age = 15;
+console.log(age);
+
+const name = "Nikita";
+console.log(name);
+
+const isStudent = true;
+console.log(isStudent);
+
+const myString = "Життя - не шахи, одного мату не вистачить";
+console.log(myString);
+
+let myNumber = 965;
+myNumber +=10;
+console.log(myNumber);
+
+const myNull = null;
+console.log(myNull);
+
+
+
+
+
+
+
+
